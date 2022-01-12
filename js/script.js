93df052548f32f0cf602def0a20a1bbed443e8e2
NISS <nishansubedi15@gmail.com>
2022-01-12 16:55:18 +0545

    scroll section active highlight in js

--- js/script.js
+++ js/script.js
@@ -107,4 +107,30 @@ function scrollUp(){
     console.log("Scroll up being called and working!")
 }
 window.addEventListener('scroll', scrollUp)
+
+
+//===================================== SCROLL SECTION ACTIVE HIGHLIGHT =========================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+
+    console.log("Section highlight working!")
+}
+window.addEventListener('scroll', scrollActive)
+
   
